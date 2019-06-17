9e1ac137f6bb35fdd4b57411cadc58f2bf6f2d75
xiaohuaye <908347816@qq.com>
2019-06-17 22:44:28 +0800

    添加功能左右可加icon

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,21 @@
 <template>
-    <button class="g-button">hi</button>
+    <button class="g-button">
+        <svg class="icon" v-if="iconName && iconPosition !== 'right'" aria-hidden="true">
+            <use :xlink:href=`#i-${iconName}`></use>
+        </svg>
+        <slot></slot>
+        <svg class="icon" v-if="iconName && iconPosition === 'right'" aria-hidden="true">
+            <use :xlink:href=`#i-${iconName}`></use>
+        </svg>
+    </button>
 </template>
 
 <script>
     export default {
-
+        props:{
+            iconName:String,
+            iconPosition: String
+        }
     }
 </script>
 
@@ -12,16 +23,19 @@
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
-        padding: 0 1em;
+        padding: 0 0.5em;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-background);
+
         &:hover {
             border-color: var(--border-color-hover);
         }
+
         &:active {
             background-color: var(--button-active-background);
         }
+
         &:focus {
             outline: none;
         }
