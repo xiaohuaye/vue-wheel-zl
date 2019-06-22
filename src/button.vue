07cb21a386226b200fcdee354bac2348d69e65aa
xiaohuaye <908347816@qq.com>
2019-06-22 09:06:43 +0800

    添加功能左右可加icon

--- src/button.vue
+++ src/button.vue
@@ -1,44 +1,51 @@
 <template>
-    <button class="g-button">
-        <svg class="icon" v-if="iconName && iconPosition !== 'right'" aria-hidden="true">
-            <use :xlink:href=`#i-${iconName}`></use>
-        </svg>
-        <slot></slot>
-        <svg class="icon" v-if="iconName && iconPosition === 'right'" aria-hidden="true">
-            <use :xlink:href=`#i-${iconName}`></use>
-        </svg>
-    </button>
+  <button class="g-button">
+    <svg class="icon" v-if="iconName && iconPosition !== 'right'" aria-hidden="true">
+      <use :xlink:href=`#i-${iconName}`></use>
+    </svg>
+    <slot></slot>
+    <svg class="icon" v-if="iconName && iconPosition === 'right'" aria-hidden="true">
+      <use :xlink:href=`#i-${iconName}`></use>
+    </svg>
+  </button>
 </template>
 
 <script>
     export default {
         props:{
             iconName:String,
-            iconPosition: String
+            iconPosition: {
+                type: String,
+              validator: function (value) {
+                  console.log(value)
+                //可查看传值内容返回true or false
+              },
+                default: 'left'
+            }
         }
     }
 </script>
 
 <style lang="scss" scoped>
-    .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 0.5em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-background);
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 0.5em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-background);
 
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-
-        &:active {
-            background-color: var(--button-active-background);
-        }
-
-        &:focus {
-            outline: none;
-        }
+    &:hover {
+      border-color: var(--border-color-hover);
     }
+
+    &:active {
+      background-color: var(--button-active-background);
+    }
+
+    &:focus {
+      outline: none;
+    }
+  }
 
 </style>
