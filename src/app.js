63f997942979374f069b3a2f3dfabb525a4faf67
xiaohuaye <908347816@qq.com>
2019-07-06 14:27:06 +0800

    添加toast组件和插件

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Footer from './footer'
 import Header from './header'
 import Layout from './layout'
 import Sider from './sider'
+import Toast from './toast'
+import PluginToast from './plugin-toast'
 
 Vue.component('g-icon',Icon);
 Vue.component('g-button',Button);
@@ -22,82 +24,31 @@ Vue.component('g-footer',Footer);
 Vue.component('g-header',Header);
 Vue.component('g-layout',Layout);
 Vue.component('g-sider',Sider);
+Vue.component('g-toast',Toast)
+Vue.use(PluginToast)
 
 
 new Vue({
-    el: '#app',
-    data:{
-        isLoading1: false,
-        isLoading2: false,
-        isLoading3: false,
-        isLoading4: false,
-        isLoading5: false,
-        message: 'hi'
-    },
-    created(){
-        /**
-         * 检测手动触发事件
-         * @file: app.js
-         * @method created
-         * @description: 使用dispatchEvent触发事件
-         * @author: zl
-         * @date: 2019/6/30
-         * @contact: 908347816@qq.com
-         */
-
-        setTimeout(()=>{
-            let event = new Event('change');
-            let inputElement = this.$el.querySelector('.testEvent input');
-            inputElement.dispatchEvent(event)
-        },3000)
-    },
-    /**
-     *  函数定义
-     *  @module function
-     */
-    methods: {
-        submitSthOne(){
-            /**
-             * submitSthOne模拟事件回调
-             * @file: app.js
-             * @method submitSthOne
-             * @description: submitSthOne模拟事件回调
-             * @return {undefined} undefined
-             * @author: zl
-             * @date: 2019/6/22
-             * @contact: 908347816@qq.com
-             */
-
-            this.isLoading1 = true;
-            setTimeout(()=>{
-                this.isLoading1 = false
-            },2000)
-        },
-        submitSthTwo(){
-            this.isLoading2 = true;
-            setTimeout(()=>{
-                this.isLoading2 = false
-            },2000)
-        },
-        submitSthThree(){
-            this.isLoading3 = true;
-            setTimeout(()=>{
-                this.isLoading3 = false
-            },2000)
-        },
-        submitSthFour(){
-            this.isLoading4 = true;
-            setTimeout(()=>{
-                this.isLoading4 = false
-            },2000)
-        },
-        submitSthFive(){
-            this.isLoading5 = true;
-            setTimeout(()=>{
-                this.isLoading5 = false
-            },2000)
-        }
+  el: '#app',
+  data:{
+    isLoading1: false,
+    isLoading2: false,
+    isLoading3: false,
+    isLoading4: false,
+    isLoading5: false,
+    message: 'hi'
+  },
+  created(){
+  },
+  /**
+   *  函数定义
+   *  @module function
+   */
+  methods: {
+    showToast(){
+      this.$toast('I am toast')
     }
+  }
 });
 
 
---
+++ src/toast.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="g-toast">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "zl-toast"
+  }
+</script>
+
+<style scoped lang="scss">
+  $font-size: 14px;
+  $toast-height: 40px;
+  $toast-bg: rgba(0,0,0,0.74);
+  .g-toast{display: flex;align-items: center;color: white;font-style: $font-size;height: $toast-height;line-height: 1.8;position: fixed;top: 0;left: 50%;transform: translateX(-50%);
+    background: $toast-bg;
+    box-shadow: 0px 0px 3px 0px rgba(0,0,0,0.50);
+    padding: 0 16px;
+    border-radius: 4px;
+  }
+</style>
